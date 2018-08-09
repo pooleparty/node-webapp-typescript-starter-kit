37fa7ae52fbf6566900edab6714354fb7d396888
Josh Poole <jpoole@vistaprint.com>
2018-08-09 07:15:23 -0400

    Added production client build file hashes and updated vendor chunks plugin

--- webpack.client.js
+++ webpack.client.js
@@ -1,63 +1,71 @@
-const path = require("path");
-const webpack = require("webpack");
-const merge = require("webpack-merge");
-const baseConfig = require("./webpack.base.js");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const indexPage = new HtmlWebpackPlugin({
   template: `!!raw-loader!${path.join(
     process.cwd(),
-    "src/index.template.ejs"
+    'src/index.template.ejs',
   )}`,
-  filename: path.resolve(__dirname, "views/index.ejs"),
+  filename: path.resolve(__dirname, 'views/index.ejs'),
   minify: {
     removeComments: true,
     collapseWhitespace: true,
-    conservativeCollapse: true
-  }
+    conservativeCollapse: true,
+  },
 });
 
 const errorPage = new HtmlWebpackPlugin({
   template: `!!raw-loader!${path.join(
     process.cwd(),
-    "src/error.template.ejs"
+    'src/error.template.ejs',
   )}`,
-  filename: path.resolve(__dirname, "views/error.ejs"),
+  filename: path.resolve(__dirname, 'views/error.ejs'),
   minify: {
     removeComments: true,
     collapseWhitespace: true,
-    conservativeCollapse: true
-  }
+    conservativeCollapse: true,
+  },
+});
+
+const vendorChunks = new webpack.optimize.CommonsChunkPlugin({
+  name: 'vendor',
+  minChunks: ({ resource }) =>
+    resource &&
+    resource.indexOf('node_modules') >= 0 &&
+    resource.match(/\.js$/),
 });
 
 const config = {
   entry: {
-    bundle: "./src/client/client.tsx"
+    bundle: './src/client/client.tsx',
   },
   output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "public/bundles"),
-    publicPath: "/bundles"
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public/bundles'),
+    publicPath: '/bundles',
   },
-  plugins: [indexPage, errorPage]
+  plugins: [vendorChunks, indexPage, errorPage],
 };
 
-const extractBundles = bundles => ({
-  plugins: bundles.map(
-    bundle => new webpack.optimize.CommonsChunkPlugin(bundle)
-  )
-});
+const prodConfig = {
+  devtool: false,
+  output: {
+    filename: '[name].[chunkhash].js',
+  },
+};
 
-module.exports = merge(
-  baseConfig,
-  config,
-  extractBundles([
-    {
-      name: "vendor",
-      minChunks: ({ resource }) =>
-        resource &&
-        resource.indexOf("node_modules") >= 0 &&
-        resource.match(/\.js$/)
-    }
-  ])
-);
+module.exports = env => {
+  let envConfig = config;
+
+  if (env === 'dist') {
+    envConfig = merge(envConfig, prodConfig);
+  }
+
+  return merge(
+    baseConfig,
+    envConfig,
+  );
+};
